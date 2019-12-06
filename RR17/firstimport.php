cf9b2f6eb5c55eb81ed997bf597d5f29483731b6
Chaitra1817 <chaitrabhatgng@gmail.com>
2019-12-06 22:19:06 +0530

    my first commit

---
+++ RR17/firstimport.php
@@ -0,0 +1,9 @@
+<?php
+$host="localhost"; // Host name
+$username="test"; // Mysql username
+$password="Welcome@123"; // Mysql password
+$db_name="test17"; // Database name
+
+$conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
+
+?>
